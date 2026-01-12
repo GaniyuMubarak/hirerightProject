92db62144932b3ee69496b6616329ba4edd0e184
GaniyuMubarak <66374487+GaniyuMubarak@users.noreply.github.com>
2026-01-12 17:04:35 +0100

    updated document

--- config/cors.php
+++ config/cors.php
@@ -4,16 +4,13 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
-<<<<<<< HEAD
     'allowed_origins' => [
             'http://localhost:8000',
             'http://127.0.0.1:8000',
             'https://hirerightapp.com'
     ],  // Allow all for testing
-=======
-    'allowed_origins' => ['*'],  // Allow all for testing
->>>>>>> 5853b763aeab113a5e8d4cb7bd2f7b6f1ab84315
+
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -24,8 +21,7 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,  // Change to false for API token auth
-<<<<<<< HEAD
+
 ];
-=======
-];
->>>>>>> 5853b763aeab113a5e8d4cb7bd2f7b6f1ab84315
+
+
